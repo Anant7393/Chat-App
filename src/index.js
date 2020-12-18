b495629c98175af874b6697284d4b4c2bfc8f05d
Abhinav Singh <thakurabhinav17122001@gmail.com>
2020-12-18 23:16:30 +0530

    :

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const server=http.createServer(app)
 const io=socketio(server)
 
 const publicPathDirectory=path.join(__dirname,'../public')
-const PORT=3002||process.env.PORT
+const PORT=process.env.PORT
 
 app.use(express.static(publicPathDirectory))
 
